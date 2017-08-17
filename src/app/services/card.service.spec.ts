3fe8a1faa3e75863816a6dfa76b8de6777134f93
Tim Duddy <timduddy@Tims-MacBook-Pro.local>
2017-08-17 16:02:18 -0700

    Update to tests

--- src/app/services/card.service.spec.ts
+++ src/app/services/card.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../../environments/environment';
